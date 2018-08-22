34697e69e90945035a7c55816c4835e09ecea162
tab0902 <mercury.brave.0902@gmail.com>
2018-08-22 21:49:09 +0900

    Fix main/rental.html

--- main/forms.py
+++ main/forms.py
@@ -5,13 +5,21 @@ from . import models
 class RentalForm(forms.ModelForm):
     class Meta:
         model = models.Reservation
-        fields = ['last_name', 'first_name', 'last_name_kana', 'last_name_kana', 'zip_code1', 'zip_code2', 'prefecture', 'city', 'address', 'email', 'gender', 'age_range']
+        fields = ['last_name', 'first_name', 'last_name_kana', 'first_name_kana', 'zip_code1', 'zip_code2', 'prefecture', 'city', 'address', 'email', 'gender', 'age_range']
+        widgets = {
+            'gender': forms.RadioSelect(),
+            'age_range': forms.RadioSelect(),
+        }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        for fieldname in ['address_name', 'address_name_kana', 'zip_code', 'prefecture', 'city', 'address', 'email']:
-            self.fields[fieldname].required = True
-        for fieldname in ['address_name', 'address_name_kana', 'zip_code', 'city', 'address', 'email']:
-            self.fields[fieldname].widget.attrs['class'] = 'in1'
         self.fields['email'].widget.attrs['placeholder'] = ' '
+        self.fields['prefecture'].empty_label = '都道府県を選択してください'
+        self.fields['zip_code1'].widget.attrs['pattern'] = '\d{3}'
+        self.fields['zip_code2'].widget.attrs['pattern'] = '\d{4}'
+
+        for fieldname in ['last_name', 'first_name', 'last_name_kana', 'first_name_kana', 'zip_code1', 'zip_code2', 'prefecture', 'city', 'address', 'email']:
+            self.fields[fieldname].required = True
+        for fieldname in ['last_name', 'first_name', 'last_name_kana', 'first_name_kana', 'zip_code1', 'zip_code2', 'city', 'address', 'email']:
+            self.fields[fieldname].widget.attrs['class'] = 'in1'
